f9887885fb0abc6540255820e6f3ad11c6ae501f
Radovan Blažek <blazra@gmail.com>
2026-04-25 21:12:35 +0200

    Introduce new `CompostCtx` struct that is passed into all the handlers
    Allocator is now passed as part of the `ctx`. `ctx` also contains the rx
    and tx messages and `user_ctx` - a void pointer passed by the user to
    `compost_msg_process()`

--- test/protocol_impl.c
+++ test/protocol_impl.c
@@ -52,7 +52,7 @@ void set_mock_date(struct MockDate* dest, time_t epoch)
         return;
     struct tm *timeinfo = gmtime(&epoch);
     dest->day = timeinfo->tm_mday;
-    dest->month = timeinfo->tm_mon + 1; 
+    dest->month = timeinfo->tm_mon + 1;
     dest->year = timeinfo->tm_year + 1900;
     char tmp[dest->as_text.len+1];
     snprintf(tmp, sizeof(tmp), "%02d%02d%04d", dest->day, dest->month, dest->year);
@@ -65,17 +65,17 @@ void set_mock_date(struct MockDate* dest, time_t epoch)
 
 uint16_t notif_request = 0xFFFF;
 
-void trigger_notification_handler(uint16_t msg_id)
+void trigger_notification_handler(struct CompostCtx *ctx, uint16_t msg_id)
 {
     notif_request = msg_id;
 }
 
-uint32_t add_int_handler(uint32_t a, uint32_t b)
+uint32_t add_int_handler(struct CompostCtx *ctx, uint32_t a, uint32_t b)
 {
     return a + b;
 }
 
-uint32_t sum_list_handler(struct CompostSliceU32 a)
+uint32_t sum_list_handler(struct CompostCtx *ctx, struct CompostSliceU32 a)
 {
     int sum = 0;
     for (int i = 0; i < a.len; i++)
@@ -83,33 +83,32 @@ uint32_t sum_list_handler(struct CompostSliceU32 a)
     return sum;
 }
 
-void void_return_handler(int16_t x)
+void void_return_handler(struct CompostCtx *ctx, int16_t x)
 {
     (void)x;
 }
 
-void void_full_handler(void)
+void void_full_handler(struct CompostCtx *ctx)
 {
     return;
 }
 
-float divide_float_handler(float a, float b)
+float divide_float_handler(struct CompostCtx *ctx, float a, float b)
 {
     return a / b;
 }
 
-struct CompostSliceU8 caesar_cipher_handler(struct CompostSliceU8 str, uint8_t offset,
-                                          struct CompostAlloc *alloc)
+struct CompostSliceU8 caesar_cipher_handler(struct CompostCtx *ctx, struct CompostSliceU8 str, uint8_t offset)
 {
-    struct CompostSliceU8 ciphertext = compost_slice_u8_new(alloc, str.len);
+    struct CompostSliceU8 ciphertext = compost_slice_u8_new(&ctx->alloc, str.len);
     for (int i = 0; i < str.len; i++)
         ciphertext.ptr[i] = str.ptr[i] + offset;
     return ciphertext;
 }
 
-struct CompostSliceU8 sort_bytes_handler(struct CompostSliceU8 data, struct CompostAlloc *alloc)
+struct CompostSliceU8 sort_bytes_handler(struct CompostCtx *ctx, struct CompostSliceU8 data)
 {
-    struct CompostSliceU8 sorted = compost_slice_u8_new(alloc, data.len);
+    struct CompostSliceU8 sorted = compost_slice_u8_new(&ctx->alloc, data.len);
     // Bubble sort algorithm
     for (int i = data.len - 1; i >= 0; i--) {
         for (int j = 0; j < data.len - 1; j++) {
@@ -126,37 +125,37 @@ struct CompostSliceU8 sort_bytes_handler(struct CompostSliceU8 data, struct Comp
     return sorted;
 }
 
-struct ListFirstAttr list_first_attr_handler(struct CompostSliceI16 data, struct CompostAlloc *alloc)
+struct ListFirstAttr list_first_attr_handler(struct CompostCtx *ctx, struct CompostSliceI16 data)
 {
-    struct ListFirstAttr ret = ListFirstAttr_init(alloc, data.len);
+    struct ListFirstAttr ret = ListFirstAttr_init(&ctx->alloc, data.len);
     compost_slice_copy(ret.data, data);
     ret.min = slice_min(ret.data);
     ret.max = slice_max(ret.data);
     return ret;
 }
 
-struct ListMidAttr list_mid_attr_handler(struct CompostSliceI16 data, struct CompostAlloc *alloc)
+struct ListMidAttr list_mid_attr_handler(struct CompostCtx *ctx, struct CompostSliceI16 data)
 {
-    struct ListMidAttr ret = ListMidAttr_init(alloc, data.len);
+    struct ListMidAttr ret = ListMidAttr_init(&ctx->alloc, data.len);
     compost_slice_copy(ret.data, data);
     ret.min = slice_min(ret.data);
     ret.max = slice_max(ret.data);
     return ret;
 }
 
-struct ListLastAttr list_last_attr_handler(struct CompostSliceI16 data, struct CompostAlloc *alloc)
+struct ListLastAttr list_last_attr_handler(struct CompostCtx *ctx, struct CompostSliceI16 data)
 {
-    struct ListLastAttr ret = ListLastAttr_init(alloc, data.len);
+    struct ListLastAttr ret = ListLastAttr_init(&ctx->alloc, data.len);
     compost_slice_copy(ret.data, data);
     ret.min = slice_min(ret.data);
     ret.max = slice_max(ret.data);
     return ret;
 }
 
-struct TwoListAttr two_list_attr_handler(struct CompostSliceI16 data_a,
-                                     struct CompostSliceI16 data_b, struct CompostAlloc *alloc)
+struct TwoListAttr two_list_attr_handler(struct CompostCtx *ctx, struct CompostSliceI16 data_a,
+                                     struct CompostSliceI16 data_b)
 {
-    struct TwoListAttr ret = TwoListAttr_init(alloc, data_a.len, data_b.len);
+    struct TwoListAttr ret = TwoListAttr_init(&ctx->alloc, data_a.len, data_b.len);
     compost_slice_copy(ret.data_a, data_a);
     compost_slice_copy(ret.data_b, data_b);
     ret.avg_a = slice_avg(ret.data_a);
@@ -165,28 +164,28 @@ struct TwoListAttr two_list_attr_handler(struct CompostSliceI16 data_a,
     return ret;
 }
 
-struct MockDate epoch_to_date_handler(int32_t epoch, struct CompostAlloc *alloc)
+struct MockDate epoch_to_date_handler(struct CompostCtx *ctx, int32_t epoch)
 {
-    struct MockDate ret = MockDate_init(alloc, MOCK_DATE_FMT_LEN, MOCK_DATE_FMT_LEN);
+    struct MockDate ret = MockDate_init(&ctx->alloc, MOCK_DATE_FMT_LEN, MOCK_DATE_FMT_LEN);
     set_mock_date(&ret, (time_t)epoch);
     return ret;
 }
 
-struct CompostSliceU8 emoji_handler(struct CompostSliceU8 text, struct CompostAlloc *alloc)
+struct CompostSliceU8 emoji_handler(struct CompostCtx *ctx, struct CompostSliceU8 text)
 {
     static uint8_t tmp[8];
     struct CompostAlloc outside_alloc = compost_alloc_init(tmp, 8);
     if (strncmp((char *)text.ptr, "😘", text.len)) {
         return compost_str_new(&outside_alloc, "🤔"); // Returning slice outside of the TX buffer
     } else {
-        return compost_str_new(alloc, "🥰"); // Returning slice inside of the TX buffer
+        return compost_str_new(&ctx->alloc, "🥰"); // Returning slice inside of the TX buffer
     }
 }
 
-struct CompostSliceU32 cat_lists_handler(struct CompostSliceU32 list_a,
-                                       struct CompostSliceU32 list_b, struct CompostAlloc *alloc)
+struct CompostSliceU32 cat_lists_handler(struct CompostCtx *ctx, struct CompostSliceU32 list_a,
+                                       struct CompostSliceU32 list_b)
 {
-    struct CompostSliceU32 ret = compost_slice_u32_new(alloc, list_a.len + list_b.len);
+    struct CompostSliceU32 ret = compost_slice_u32_new(&ctx->alloc, list_a.len + list_b.len);
     for (int i = 0; i < list_a.len; i++) {
         compost_slice_set(ret, i, compost_slice_get(list_a, i));
     }
@@ -196,9 +195,9 @@ struct CompostSliceU32 cat_lists_handler(struct CompostSliceU32 list_a,
     return ret;
 }
 
-struct MockLfsr get_random_number_handler(uint64_t seed, uint8_t iter, struct CompostAlloc *alloc)
+struct MockLfsr get_random_number_handler(struct CompostCtx *ctx, uint64_t seed, uint8_t iter)
 {
-    struct MockLfsr lfsr = MockLfsr_init(alloc, MOCK_DATE_FMT_LEN, MOCK_DATE_FMT_LEN);
+    struct MockLfsr lfsr = MockLfsr_init(&ctx->alloc, MOCK_DATE_FMT_LEN, MOCK_DATE_FMT_LEN);
     lfsr.polynomial = 0xD800000000000000; //< Copy this value when reimplementing this test
     // LFSR does not work for seed=0 -> store any other value
     lfsr.value = seed == 0 ? 0x1F2E3D4C5B6A7988 : seed;
@@ -221,12 +220,12 @@ struct MockLfsr get_random_number_handler(uint64_t seed, uint8_t iter, struct Co
 
 uint8_t tx_buf_notif[1024];
 
-void notify_date_handler(struct MockDate date)
+void notify_date_handler(struct CompostCtx *ctx, struct MockDate date)
 {
     (void)date; // Unused
 }
 
-void notify_motor_control_handler(struct MockMotorControl control)
+void notify_motor_control_handler(struct CompostCtx *ctx, struct MockMotorControl control)
 {
     assert(control.state == MOTOR_STATE_ON);
     assert(control.direction == MOTOR_DIRECTION_UP);
@@ -250,13 +249,13 @@ void notify_motor_control_handler(struct MockMotorControl control)
     }
 }
 
-void notify_motor_report_handler(struct MockMotorReport report)
+void notify_motor_report_handler(struct CompostCtx *ctx, struct MockMotorReport report)
 {
     assert(report.state == MOTOR_STATE_START);
     assert(report.direction == MOTOR_DIRECTION_UP);
     assert(report.voltage.len == 20);
     assert(report.current.len == 20);
-    
+
     // Send back a notification as an async response
     struct MockMotorControl control = MockMotorControl_init();
     control.state = MOTOR_STATE_STOP;
@@ -271,14 +270,14 @@ void notify_motor_report_handler(struct MockMotorReport report)
     }
 }
 
-void struct_in_param_handler(struct ListFirstAttr structure)
+void struct_in_param_handler(struct CompostCtx *ctx, struct ListFirstAttr structure)
 {
     assert(structure.data.len == 10);
     assert(structure.min == 1);
     assert(structure.max == 10);
 }
 
-void notify_bitfields_handler(struct BitfieldStruct config)
+void notify_bitfields_handler(struct CompostCtx *ctx, struct BitfieldStruct config)
 {
     config.ccm = ~config.ccm;
     config.channel = ~config.channel;
@@ -295,7 +294,7 @@ void notify_bitfields_handler(struct BitfieldStruct config)
     fflush(stdout);
 }
 
-void notify_log_handler(struct MockLogMessage log)
+void notify_log_handler(struct CompostCtx *ctx, struct MockLogMessage log)
 {
     (void)log;
 }
